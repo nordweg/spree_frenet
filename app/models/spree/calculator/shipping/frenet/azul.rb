abf58f625eeda9fab56b7896894ce342c1c049bf
Lucas Kuhn <lucasalexkuhn@gmail.com>
2021-08-02 13:19:39 -0300

    Changes azul service code, adds files that should go to gem

--- app/models/spree/calculator/shipping/frenet/azul.rb
+++ app/models/spree/calculator/shipping/frenet/azul.rb
@@ -2,7 +2,7 @@ require_dependency 'spree/calculator'
 
 module Spree::Calculator::Shipping::Frenet
   class Azul < Spree::Calculator::Shipping::Frenet::Base
-    FRENET_SERVICE_CODE = 'Azul_WS'
+    FRENET_SERVICE_CODE = 'AZUL_02'
 
     def self.description
       "Frenet - Azul Cargo"
